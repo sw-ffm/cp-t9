048c166ca45be6c00d6cf7ba1dd76aa981fa6c87
Stefan Watzinger <stefanwatzingerfrankfurt@gmail.com>
2024-08-23 18:29:29 +0200

    varname

--- config/container.php
+++ config/container.php
@@ -4,7 +4,7 @@ use Stefan\CpTn\TNine;
 
 return [
     TNine::class => function(){
-        $key_data = require "keyassignments.php";
-        return new TNine($key_data);
+        $key_assignments = require "keyassignments.php";
+        return new TNine($key_assignments);
     }
 ];
